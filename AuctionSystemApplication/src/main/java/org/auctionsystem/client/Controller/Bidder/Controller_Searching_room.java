45019263055dafca1bbe79d0b92998dd3f93335a
hainam2k7 <25021904@vnu.edu.vn>
2026-05-10 09:31:22 +0700

    xóa ảnh kính lúp dư thừa

--- AuctionSystemApplication/src/main/java/org/auctionsystem/client/Controller/Bidder/Controller_Searching_room.java
+++ AuctionSystemApplication/src/main/java/org/auctionsystem/client/Controller/Bidder/Controller_Searching_room.java
@@ -1,4 +1,20 @@
 package org.auctionsystem.client.Controller.Bidder;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import org.auctionsystem.client.Controller.Scene_Utils;
+
+import java.io.IOException;
+
 public class Controller_Searching_room {
+    //điều kiện để tìm kiếm sản phẩm: nhập tên sản phẩm + chọn loại sản phẩm
+    //thiếu 1 trong 2 là không được
+    @FXML
+    public void Back_to_Dashboard(ActionEvent event) {
+        try {
+            Scene_Utils.Change_Scene(event, "/org/auctionsystem/client/View/Bidder_Dashboard.fxml");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
